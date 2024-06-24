360be2938b36fa7aa975dfc4bd62812f18b3125f
Zach <zraisnorman@gmail.com>
2024-06-24 14:54:44 -0400

    fix sliders

--- src/routes/events/[slug]/meta-form.svelte
+++ src/routes/events/[slug]/meta-form.svelte
@@ -1,8 +1,10 @@
 
 <script lang="ts" context="module">
 	import { z } from "zod";
-	export const metaFormSchema = z.object({
-		title: z
+  import { objWithRequirements } from "$lib/components/eventInputs/requirements-form.svelte";
+
+  export const metaFormSchema = objWithRequirements.extend({
+    title: z
 			.string()
 			.min(6, "Title must be at least 6 characters.")
 			.max(60, "Title must not be longer than 60 characters"),
@@ -15,36 +17,50 @@
     random: z.boolean(),
     priority: z.number().int().min(0).max(100),
     rarity: z.number().int().min(0).max(100),
-	});
+  })
+	// export const metaFormSchema = z.object({
+	// 	title: z
+	// 		.string()
+	// 		.min(6, "Title must be at least 6 characters.")
+	// 		.max(60, "Title must not be longer than 60 characters"),
+	// 	slug: z
+  //     .string({ required_error: "Set a valid slug" })
+  //     .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, "Slug can only be lowercase letters and numbers separated by hyphens")
+  //     .min(6, "Slug must be at least 6 characters")
+  //     .max(60, "Slug must not be longer than 60 characters"),
+  //   repeatable: z.boolean(),
+  //   random: z.boolean(),
+  //   priority: z.number().int().min(0).max(100),
+  //   rarity: z.number().int().min(0).max(100),
+	// });
 
-  // I think something here is uneccessary because of no serverside
 	export type MetaFormSchema = typeof metaFormSchema;
 </script>
 
 <script lang='ts'>
-  import { type Infer, type SuperValidated, superForm } from "sveltekit-superforms";
+  import { superForm } from "sveltekit-superforms";
 	import SuperDebug from "sveltekit-superforms";
 	import { zodClient } from "sveltekit-superforms/adapters";
   import { Switch } from "$lib/components/ui/switch/index.js";
-  import * as Alert from "$lib/components/ui/alert/index.js";
   import {Input} from "$lib/components/ui/input/index.js";
   import * as Form from "$lib/components/ui/form/index.js";
   import {Slider} from "$lib/components/ui/slider/index.js";
-	import * as Select from "$lib/components/ui/select/index.js";
-	import { Textarea } from "$lib/components/ui/textarea/index.js";
-	import { cn } from "$lib/utils.js";
-	import { browser } from "$app/environment";
+  import {Separator} from "$lib/components/ui/separator/index";
+  import RequirementsForm from "$lib/components/eventInputs/requirements-form.svelte";
+
+  import { browser } from "$app/environment";
 
   import { currentEvent } from "$lib/stores/eventStore.js";
   import { valid } from "$lib/stores/uiStore";
-	import { ExclamationTriangle } from "svelte-radix";
 	import { tick } from "svelte";
+	import type { Event } from "$lib/classes/eventClasses";
 
   // export let data: SuperValidated<Infer<MetaFormSchema>>;
 
-  let { title, slug, repeatable, random, priority, rarity, requires } = $currentEvent.meta;
+  let current = $currentEvent.meta;
 
-  const form = superForm({ title, slug, repeatable, random, priority, rarity }, {
+  const form = superForm( { ...current }, {
+    dataType: 'json',
 		validators: zodClient(metaFormSchema),
 	});
 
@@ -55,6 +71,7 @@
     clearTimeout(saveTimer);
     tick().then(() => {
       saveTimer = setTimeout(() => {
+        console.log(metaFormSchema.safeParse($formData))
         if (metaFormSchema.safeParse($formData).success) {
           let ce = $currentEvent;
           ce.meta = { ...ce.meta, ...$formData};
@@ -66,6 +83,22 @@
       }, 50);
     });
   }
+
+  // function handleAutoSave(currentEvent: Event, editing: any, schema: any, data: any, timer: ReturnType<typeof setTimeout>) {
+  //   clearTimeout(timer);
+  //   tick().then(() => {
+  //     timer = setTimeout(() => {
+  //       if (schema.safeParse(data).success) {
+  //         let ce = $currentEvent;
+  //         editing = { ...editing, ...data};
+  //         currentEvent.set(ce);
+  //         valid.set(true);
+  //       } else {
+  //         valid.set(false);
+  //       }
+  //     }, 50);
+  //   });
+  // }
 
   function slugGen() {
     if ($formData.title && !$formData.slug) {
@@ -115,72 +148,77 @@
     </Form.Description>
     <Form.FieldErrors />
   </Form.Field>
+
+  <Form.Field {form} name="repeatable">
+    <Form.Control let:attrs>
+      <Form.Label>Repeatable</Form.Label>
+      <Switch 
+        {...attrs} 
+        bind:checked={$formData.repeatable}
+        on:click={saveMeta}
+      />
+    </Form.Control>
+    <Form.Description>
+      Whether or not this event is repeatable (can be played multiple times in a single game)
+    </Form.Description>
+    <Form.FieldErrors />
+  </Form.Field>
+
+  <Form.Field {form} name="random">
+    <Form.Control let:attrs>
+      <Form.Label>Random</Form.Label>
+      <Switch 
+        {...attrs} 
+        bind:checked={$formData.random}
+        on:click={saveMeta}
+        
+      />
+    </Form.Control>
+    <Form.Description>
+      Whether or not this event can be selected at random in the game when event is scheduled.
+    </Form.Description>
+    <Form.FieldErrors />
+  </Form.Field>
+
+  <Form.Field {form} name="priority" on:click={handleSlider}>
+    <Form.Control let:attrs>
+      <Form.Label>Priority</Form.Label> <span>{aPriority}</span>
+      <Slider 
+        max={100} 
+        step={1}
+        {...attrs}
+        bind:value={aPriority}
+        onValueChange={handleSlider}
+      />
+    </Form.Control>
+    <Form.Description>
+      The priority of this event. Higher priority events are more likely to be selected when random, and will take precedence in scheduling conflicts.
+    </Form.Description>
+    <Form.FieldErrors />
+  </Form.Field>
+
+  <Form.Field {form} name="rarity" on:click={handleSlider}>
+    <Form.Control let:attrs>
+      <Form.Label>Rarity</Form.Label> <span>{aRarity}</span>
+      <Slider 
+        max={100} 
+        step={1}
+        {...attrs}
+        bind:value={aRarity}
+        onValueChange={handleSlider}
+      />
+    </Form.Control>
+    <Form.Description>
+      The rarity of this event. Higher rarity events are less likely to be selected at random.
+    </Form.Description>
+    <Form.FieldErrors />
+  </Form.Field>
+
+  <Separator />
+
+  <RequirementsForm form={form} />
+
 </form>
-
-<Form.Field {form} name="repeatable">
-  <Form.Control let:attrs>
-    <Form.Label>Repeatable</Form.Label>
-    <Switch 
-      {...attrs} 
-      bind:checked={$formData.repeatable}
-      on:click={saveMeta}
-    />
-  </Form.Control>
-  <Form.Description>
-    Whether or not this event is repeatable (can be played multiple times in a single game)
-  </Form.Description>
-  <Form.FieldErrors />
-</Form.Field>
-
-<Form.Field {form} name="random">
-  <Form.Control let:attrs>
-    <Form.Label>Random</Form.Label>
-    <Switch 
-      {...attrs} 
-      bind:checked={$formData.random}
-      on:click={saveMeta}
-      
-    />
-  </Form.Control>
-  <Form.Description>
-    Whether or not this event can be selected at random in the game when event is scheduled.
-  </Form.Description>
-  <Form.FieldErrors />
-</Form.Field>
-
-<Form.Field {form} name="priority" on:click={handleSlider}>
-  <Form.Control let:attrs>
-    <Form.Label>Priority</Form.Label> <span>{aPriority}</span>
-    <Slider 
-      max={100} 
-      step={1}
-      {...attrs}
-      bind:value={aPriority}
-      onValueChange={handleSlider}
-    />
-  </Form.Control>
-  <Form.Description>
-    The priority of this event. Higher priority events are more likely to be selected when random, and will take precedence in scheduling conflicts.
-  </Form.Description>
-  <Form.FieldErrors />
-</Form.Field>
-
-<Form.Field {form} name="rarity" on:click={handleSlider}>
-  <Form.Control let:attrs>
-    <Form.Label>Rarity</Form.Label> <span>{aRarity}</span>
-    <Slider 
-      max={100} 
-      step={1}
-      {...attrs}
-      bind:value={aRarity}
-      onValueChange={handleSlider}
-    />
-  </Form.Control>
-  <Form.Description>
-    The rarity of this event. Higher rarity events are less likely to be selected at random.
-  </Form.Description>
-  <Form.FieldErrors />
-</Form.Field>
 
 {#if browser}
 	<SuperDebug data={$formData} />
