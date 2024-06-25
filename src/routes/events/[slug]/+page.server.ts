5759a0db8e76ef14d6432de8e05b16b32b53e7af
Zach <zraisnorman@gmail.com>
2024-06-25 16:36:41 -0400

    unused page.server

--- src/routes/events/[slug]/+page.server.ts
+++ src/routes/events/[slug]/+page.server.ts
@@ -1,25 +1,27 @@
-import { superValidate } from "sveltekit-superforms";
-import { zod } from "sveltekit-superforms/adapters";
-import { type Actions, fail } from "@sveltejs/kit";
-import { metaFormSchema } from "$lib/components/eventInputs/meta/meta-form.svelte";
-import type { PageServerLoad } from "./$types.js";
+// no page.server.ts - or server-side validation - for now!
 
-export const load: PageServerLoad = async () => {
-	return {
-		form: await superValidate(zod(metaFormSchema)),
-	};
-};
+// import { superValidate } from "sveltekit-superforms";
+// import { zod } from "sveltekit-superforms/adapters";
+// import { type Actions, fail } from "@sveltejs/kit";
+// import { metaFormSchema } from "$lib/components/eventInputs/meta/meta-form.svelte";
+// import type { PageServerLoad } from "./$types.js";
 
-export const actions: Actions = {
-	default: async (event) => {
-		const form = await superValidate(event, zod(metaFormSchema));
-		if (!form.valid) {
-			return fail(400, {
-				form,
-			});
-		}
-		return {
-			form,
-		};
-	},
-};
+// export const load: PageServerLoad = async () => {
+// 	return {
+// 		form: await superValidate(zod(metaFormSchema)),
+// 	};
+// };
+
+// export const actions: Actions = {
+// 	default: async (event) => {
+// 		const form = await superValidate(event, zod(metaFormSchema));
+// 		if (!form.valid) {
+// 			return fail(400, {
+// 				form,
+// 			});
+// 		}
+// 		return {
+// 			form,
+// 		};
+// 	},
+// };
