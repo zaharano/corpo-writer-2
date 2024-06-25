1776ba807a3a23994023a657171199ba3e1397d7
Zach <zraisnorman@gmail.com>
2024-06-24 21:13:40 -0400

    start screen menu

---
+++ src/routes/events/[slug]/screens/+page.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+	import Heading from "$lib/components/ui/typography/heading.svelte";
+  import {Separator} from "$lib/components/ui/separator/index";
+  import ScreenTable from "$lib/components/eventDisplay/screen-table.svelte";
+</script>
+
+<div class="space-y-6">
+  <div>
+    <Heading level={3}>Screens</Heading>
+    <p class="text-sm text-muted-foreground">The screens that make up this event</p>
+  </div>
+  <Separator />
+</div>
