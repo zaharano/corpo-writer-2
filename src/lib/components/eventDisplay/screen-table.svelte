5a104b2d6ce5281a19d95d8982089e3ac80908ee
Zach <zraisnorman@gmail.com>
2024-06-24 21:12:12 -0400

    start screen table

---
+++ src/lib/components/eventDisplay/screen-table.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+  import { currentEvent } from "$lib/stores/eventStore";
+  import * as Table from "$lib/components/ui/table/index.js";
+  import StatusBadge from "$lib/components/eventDisplay/statusBadge.svelte";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Check } from "lucide-svelte";
+  import * as Popover from "$lib/components/ui/popover";
+  import * as Select from "$lib/components/ui/select";
+
+  let { screens } = $currentEvent;
+</script>
+
