79561ae82c1ba2c2d886b7c9407e4170b2fcb4e9
Zach <zraisnorman@gmail.com>
2024-06-26 10:46:45 -0400

    name updates in main page file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script lang='ts'>
   import Heading from "$lib/components/ui/typography/heading.svelte";
   import settings from "$lib/settings";
-	import EventTable from "$lib/components/eventDisplay/eventTable.svelte";
-	import NewEventDialog from "$lib/components/eventInputs/event/newEventDialog.svelte";
-
+	import EventTable from "$lib/components/eventDisplay/event-table.svelte";
+  import NewThingDialog from "$lib/components/eventInputs/event/new-thing-dialog.svelte";
+  
   const { gameTitle } = settings;
 
 </script>
 
-<Heading className="text-center" level={1}>Let's write <em>{gameTitle}</em></Heading>
+<Heading className="mt-10 text-center" level={1}>Let's write <em>{gameTitle}</em></Heading>
 
 <div class="my-4 w-full flex items-center justify-center">
-  <NewEventDialog />
+  <NewThingDialog purpose="event" />
 </div>
 
 <EventTable />
