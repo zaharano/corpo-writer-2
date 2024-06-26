8c0e418de104689b13a30e77491e99b248bfb74a
Zach <zraisnorman@gmail.com>
2024-06-26 10:43:58 -0400

    rename table for new scheme - add alert dialog to delete

--- src/lib/components/eventDisplay/event-table.svelte
+++ src/lib/components/eventDisplay/event-table.svelte
@@ -5,6 +5,7 @@
   import { Check } from "lucide-svelte";
   import * as Popover from "$lib/components/ui/popover";
   import * as Select from "$lib/components/ui/select";
+  import * as AlertDialog from "$lib/components/ui/alert-dialog";
 
   import { eventStore } from "$lib/stores/eventStore";
 
@@ -70,10 +71,27 @@
         </Table.Cell>
         <Table.Cell class="text-right">{Object.keys(event.screens).length}</Table.Cell>
         <Table.Cell class="max-w-8">
-          <Button on:click={() => eventStore.removeEvent(event.id)}>Delete</Button>
+           
           <a href={"/events/"+ event.meta.slug}>
             <Button>Edit</Button>
           </a>
+
+          <AlertDialog.Root>
+            <AlertDialog.Trigger><Button variant="destructive">Delete</Button></AlertDialog.Trigger>
+            <AlertDialog.Content>
+              <AlertDialog.Header>
+                <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+                <AlertDialog.Description>
+                  This action cannot be undone. This will permanently delete this event and all of its contents.
+                </AlertDialog.Description>
+              </AlertDialog.Header>
+              <AlertDialog.Footer>
+                <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+                <AlertDialog.Action><Button on:click={() => eventStore.removeEvent(event.id)}>Delete</Button></AlertDialog.Action>
+              </AlertDialog.Footer>
+            </AlertDialog.Content>
+          </AlertDialog.Root>
+
         </Table.Cell>
       </Table.Row>
     {/each}
