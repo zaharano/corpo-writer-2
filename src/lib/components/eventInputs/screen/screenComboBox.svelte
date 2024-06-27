501e4e54da3f4d1c09a19d3a537156d5cd6017ff
Zach <zraisnorman@gmail.com>
2024-06-27 09:30:08 -0400

    combobox for screen select

---
+++ src/lib/components/eventInputs/screen/screenComboBox.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import Check from "lucide-svelte/icons/check";
+  import ChevronsUpDown from "lucide-svelte/icons/chevrons-up-down";
+  import * as Command from "$lib/components/ui/command/index.js";
+  import * as Popover from "$lib/components/ui/popover/index.js";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { cn } from "$lib/utils.js";
+  import { tick } from "svelte";
+
+  import { currentEvent } from "$lib/stores/eventStore";
+ 
+  const screens = currentEvent.allSimplifiedScreens();
+ 
+  let open = false;
+  let value = "";
+ 
+  $: selectedValue =
+    screens.find((f) => f.id === value)?.name ??
+    "Select next screen...";
+ 
+  // We want to refocus the trigger button when the user selects
+  // an item from the list so users can continue navigating the
+  // rest of the form with the keyboard.
+  function closeAndFocusTrigger(triggerId: string) {
+    open = false;
+    tick().then(() => {
+      document.getElementById(triggerId)?.focus();
+    });
+  }
+</script>
+ 
+<Popover.Root bind:open let:ids>
+  <Popover.Trigger asChild let:builder>
+    <Button
+      builders={[builder]}
+      variant="outline"
+      role="combobox"
+      aria-expanded={open}
+      class="w-[200px] justify-between"
+    >
+      {selectedValue}
+      <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+    </Button>
+  </Popover.Trigger>
+  <Popover.Content class="w-[200px] p-0">
+    <Command.Root>
+      <Command.Input placeholder="Search screens..." />
+      <Command.Empty>Create screen stub</Command.Empty>
+      <Command.Group>
+        {#each screens as screen}
+          <Command.Item
+            value={screen.id}
+            onSelect={(currentValue) => {
+              value = currentValue;
+              closeAndFocusTrigger(ids.trigger);
+            }}
+          >
+            <Check
+              class={cn(
+                "mr-2 h-4 w-4",
+                value !== screen.id && "text-transparent"
+              )}
+            />
+            {screen.name}
+          </Command.Item>
+        {/each}
+      </Command.Group>
+    </Command.Root>
+  </Popover.Content>
+</Popover.Root>
