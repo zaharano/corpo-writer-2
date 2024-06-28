2fa406c2dede083d4caf775105ec7b27442ce5b0
Zach <zraisnorman@gmail.com>
2024-06-27 22:18:37 -0400

    remove hidden on mobile

--- src/routes/events/[slug]/+layout.svelte
+++ src/routes/events/[slug]/+layout.svelte
@@ -37,7 +37,7 @@
 	];
 </script>
 
-<div class="hidden space-y-6 p-10 pb-16 md:block">
+<div class="space-y-6 p-10 pb-16">
 	<div class="space-y-0.5">
 		<h2 class="text-2xl font-bold tracking-tight">Editing <span class="bg-muted text-muted-foreground rounded-sm px-2 py-1">{ce?.meta.title}</span></h2>
 		<p class="text-muted-foreground">
