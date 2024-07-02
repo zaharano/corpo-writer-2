ff0f734914f829e1271f6a5fc6dfe8e6f9fe4d0e
Zach <zraisnorman@gmail.com>
2024-07-02 17:13:04 -0400

    allow full width to ddialog button. allow slotted delete. allow name for thing being deleted.

--- src/lib/components/eventInputs/event/delete-dialog.svelte
+++ src/lib/components/eventInputs/event/delete-dialog.svelte
@@ -2,24 +2,27 @@
   import * as AlertDialog from "$lib/components/ui/alert-dialog";
   import { Button } from "$lib/components/ui/button/index.js";
 
-  export let handleDelete: () => void;
+  export let handleDelete: () => void = () => {};
+  export let thingToDelete: string = 'event'
 
   let className: string | undefined = undefined;
   export { className as class };
 </script>
 
 <AlertDialog.Root>
-  <AlertDialog.Trigger><Button class={className} variant="destructive">Delete</Button></AlertDialog.Trigger>
+  <AlertDialog.Trigger class={className}><Button class={className} variant="destructive">Delete</Button></AlertDialog.Trigger>
   <AlertDialog.Content>
     <AlertDialog.Header>
       <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
       <AlertDialog.Description>
-        This action cannot be undone. This will permanently delete this event and all of its contents.
+        This action cannot be undone. This will permanently delete this {thingToDelete} and all of its contents.
       </AlertDialog.Description>
     </AlertDialog.Header>
     <AlertDialog.Footer>
       <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-      <AlertDialog.Action><Button on:click={handleDelete}>Delete</Button></AlertDialog.Action>
+      <AlertDialog.Action>
+        <slot><Button on:click={handleDelete}>Delete</Button></slot>
+      </AlertDialog.Action>
     </AlertDialog.Footer>
   </AlertDialog.Content>
 </AlertDialog.Root>
