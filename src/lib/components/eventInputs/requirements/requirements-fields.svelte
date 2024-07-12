d2505c10c7bcb954080cafab8b3030559b75f85d
Zach <zraisnorman@gmail.com>
2024-07-11 21:46:07 -0400

    remove stuff moved to the component

--- src/lib/components/eventInputs/requirements/requirements-fields.svelte
+++ src/lib/components/eventInputs/requirements/requirements-fields.svelte
@@ -5,7 +5,7 @@
   import { setFlagSchema } from "$lib/components/eventInputs/flags/set-flags.svelte";
 
   const requires = objWithGameRequirements.extend({
-      flags: z.array(setFlagSchema),
+      flags: setFlagSchema,
     });
 
 	export const objWithRequirements = z.object({
@@ -24,46 +24,19 @@
 <script lang="ts">
   import * as Form from "$lib/components/ui/form/index.js";
 	import GameRequirementsForm from "./game-requirements-fields.svelte";
-  import ComboBox from "../comboBox.svelte";
-
-  import { flagStore } from "$lib/stores";
-	import type { ID, SetFlag } from "$lib/classes/eventClasses";
-	import { Button } from "$lib/components/ui/button";
-	import { Switch } from "$lib/components/ui/switch";
-	import { CircleXIcon } from "lucide-svelte";
-	import { Label } from "$lib/components/ui/label";
 	import SetFlags from "../flags/set-flags.svelte";
-	import { set } from "zod";
 
   export let form;
   export let context = "event";
 
   const { form: formData } = form;
 
-  let addedFlag: ID | undefined = undefined;
-  $: addedFlag && addFlag();
-
-  function addFlag() {
-    $formData.requires.flags = [...$formData.requires.flags, { id: addedFlag, value: false }];
-    addedFlag = undefined;
-  }
-
-  // let allFlags = flagStore.allSimplifiedFlags();
+  $: console.log($formData.requires.flags);
 </script>
 
 <Form.Field {form} name="flags">
   <Form.Control let:attrs>
     <Form.Label>Flags</Form.Label><br>
-    <!-- <ComboBox bind:value={addedFlag} purpose='flag' targets={flagStore.allSimplifiedFlags()}/>
-    {#if $formData.requires.flags.length > 0}
-      {#each $formData.requires.flags as flag, i}
-        <div class='flex items-center gap-x-4'>
-          <span class='flex-grow'>{flagStore.getFlagTitle(flag.id)}</span>
-          <span><Label>True?</Label> <Switch bind:checked={$formData.requires.flags[i].value} /></span>
-          <Button size='icon' variant='ghost' on:click={() => $formData.requires.flags = $formData.requires.flags.filter(f => f.id !== flag.id)}><CircleXIcon /></Button>
-        </div>
-      {/each}
-    {/if} -->
     <SetFlags bind:setFlags={$formData.requires.flags} />
   </Form.Control>
   <Form.Description>
@@ -72,4 +45,4 @@
   <Form.FormFieldErrors />
 </Form.Field>
 
-<GameRequirementsForm {form} context={context} />
+<GameRequirementsForm {form} {context} />
