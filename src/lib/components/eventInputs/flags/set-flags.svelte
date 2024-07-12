a44caea682fafda7d6f33f58b2cb52c17a1cdf80
Zach <zraisnorman@gmail.com>
2024-07-12 17:07:30 -0400

    animate setflags, filter out already set

--- src/lib/components/eventInputs/flags/set-flags.svelte
+++ src/lib/components/eventInputs/flags/set-flags.svelte
@@ -19,6 +19,7 @@
 
 	import type { ID, SetFlag } from "$lib/classes/eventClasses";
 	import { Separator } from "$lib/components/ui/separator";
+	import { slide } from 'svelte/transition';
 
   export let setFlags: SetFlag[];
 
@@ -30,19 +31,23 @@
     addedFlag = undefined;
   }
 
+  $: flagOptions = flagStore.allSimplifiedFlags().filter(f => !setFlags.map(f => f.id).includes(f.id));
   // TODO: keep flags in combobox synced with the store
 </script>
 
-<ComboBox bind:value={addedFlag} purpose='flag' targets={flagStore.allSimplifiedFlags()}/>
+<ComboBox bind:value={addedFlag} purpose='flag' targets={flagOptions}/>
 {#if setFlags.length > 0}
-  {#each setFlags as flag, i}
-    {#if i > 0}
-      <Separator />
-    {/if}
-    <div class='flex items-center gap-x-4'>
-      <span class='flex-grow text-sm font-medium leading-none'>{flagStore.getFlagTitle(flag.id)}</span>
-      <span class='flex items-center gap-2'><Label>True?</Label> <Switch bind:checked={setFlags[i].value} /></span>
-      <Button size='icon' variant='ghost' on:click={() => setFlags = setFlags.filter(f => f.id !== flag.id)}><CircleXIcon /></Button>
-    </div>
-  {/each}
+  <div transition:slide>
+    {#each setFlags as flag, i (flag.id)}
+      {#if i > 0}
+        <Separator />
+      {/if}
+      <div transition:slide class='flex items-center gap-x-4'>
+        <span class='flex-grow text-sm font-medium leading-none'>{flagStore.getFlagTitle(flag.id)}</span>
+        <span class='flex items-center gap-2'><Label>True?</Label> <Switch bind:checked={setFlags[i].value} /></span>
+        <Button size='icon' variant='ghost' on:click={() => setFlags = setFlags.filter(f => f.id !== flag.id)}><CircleXIcon /></Button>
+      </div>
+    {/each}
+  </div>
+
 {/if}
